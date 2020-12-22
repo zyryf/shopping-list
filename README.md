977b718276f7a87290ce7061c97490096040fcad
Szymon Borkowski <szymon.borkowski@gmail.com>
2020-12-22 22:49:28 +0100

    add loader

--- README.md
+++ README.md
@@ -14,4 +14,12 @@ npm start
 
 ```
 npm run build
+
+### Info
+```
+
+**This project uses [MDB Webpack Starter boilerplate](https://github.com/mdbootstrap/mdb-webpack-starter)**
+
+```
+
 ```
--- src/js/events/initalLoad.js
+++ src/js/events/initalLoad.js
@@ -4,6 +4,9 @@ import UI from '../classes/UI';
 const initialLoad = document.addEventListener('DOMContentLoaded', () => {
   const products = Storage.getProducts();
 
+  const loader = document.querySelector('.loader-wrapper');
+  loader.style = 'display:none';
+
   products.forEach((product) => {
     UI.addProduct(product, null);
   });
